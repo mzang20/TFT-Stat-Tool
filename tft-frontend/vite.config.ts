0b6c1382e8eeda0cf8d2397a909fbbe713c853d0
Michael Zang <mingjiazang@gmail.com>
2025-07-19 16:35:48 -0400

    Updated Set 14

--- tft-frontend/vite.config.ts
+++ tft-frontend/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/TFT-Stat-Tool/', // 👈 important
+  base: '/TFT-Stat-Tool/',
   plugins: [react()],
 });
